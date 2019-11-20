1ef1dc4405d39b361a94c85cf30fe9d7f7a5a44b
Narek Avagyan <ava.narek@gmail.com>
2019-11-21 01:16:10 +0300

    +

--- src/components/RemoteVideoPlaceholder.jsx
+++ src/components/RemoteVideoPlaceholder.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import Loop from '@material-ui/icons/Loop';
+import React, { Component } from "react";
+import Loop from "@material-ui/icons/Loop";
 
 export default class RemoteVideoPlaceholder extends Component {
   constructor(props) {
@@ -23,12 +23,15 @@ export default class RemoteVideoPlaceholder extends Component {
       return (
         <div>
           <h1>
-            searching...{' '}
+            searching...{" "}
             <span role="img" aria-label="thinking">
               &#129300;
-            </span>{' '}
+            </span>{" "}
           </h1>
-          <Loop className="rotating" style={{ width: '50px', height: '50px' }} />
+          <Loop
+            className="rotating"
+            style={{ width: "50px", height: "50px" }}
+          />
         </div>
       );
     }
@@ -37,14 +40,14 @@ export default class RemoteVideoPlaceholder extends Component {
       return (
         <div>
           <h1>
-            Stranger disconnected{' '}
+            Stranger disconnected{" "}
             <span role="img" aria-label="surprised">
               &#128550;
             </span>
           </h1>
           <h3>
-            Click 'next' for continue!{' '}
-            <span role="img" aria-label="badboy">
+            Click 'next' for continue!{" "}
+            <span role="img" aria-label="cool">
               &#128526;
             </span>
           </h3>
@@ -56,7 +59,7 @@ export default class RemoteVideoPlaceholder extends Component {
       return (
         <div>
           <h1>
-            Welcome to VideoChat Roulette!{' '}
+            Welcome to VideoChat Roulette!{" "}
             <span role="img" aria-label="tongue">
               &#128539;
             </span>
@@ -69,8 +72,18 @@ export default class RemoteVideoPlaceholder extends Component {
 
   render() {
     return (
-      <div style={{ height: '100%', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-        <div style={{ textAlign: 'center', color: '#fafafa' }}>{this.renderPlaceholder()}</div>
+      <div
+        style={{
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center"
+        }}
+      >
+        <div style={{ textAlign: "center", color: "#fafafa" }}>
+          {this.renderPlaceholder()}
+        </div>
       </div>
     );
   }
---
+++ src/devServer.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log('Server listening at port %d', port);
+});
+
+let freeClients = new Set();
+let searchingClients = new Set();
+
+io.on('connection', socket => {
+  freeClients.add(socket.id);
+  socket.broadcast.emit('online', io.sockets.clients().server.eio.clientsCount);
+
+  socket.on('message', entry => {
+    const data = { sender: socket.id, msg: entry.msg };
+    socket.emit('message', data);
+    socket.to(entry.id).emit('message', data);
+  });
+
+  socket.on('signal', ({ signal, toId }) => {
+    socket.to(toId).emit('signal', signal);
+  });
+
+  socket.on('typing', toId => {
+    socket.emit('typing', socket.id);
+    socket.to(toId).emit('typing', socket.id);
+  });
+
+  socket.on('stop typing', toId => {
+    socket.emit('stop typing');
+    socket.to(toId).emit('stop typing');
+  });
+
+  socket.on('searching', toId => {
+    if (toId) {
+      socket.to(toId).emit('stranger disconnected');
+      socket.emit('stranger disconnected');
+    } else {
+      freeClients.delete(socket.id);
+    }
+    searchingClients.add(socket.id);
+    const inSearch = Array.from(searchingClients);
+    if (inSearch.length >= 2) {
+      const toId = inSearch[0] !== socket.id ? inSearch[0] : inSearch[1];
+      socket.emit('connected', { toId, initiator: false });
+      socket.to(toId).emit('connected', { toId: socket.id, initiator: true });
+      searchingClients.delete(socket.id);
+      searchingClients.delete(toId);
+    }
+  });
+
+  socket.on('stop', toId => {
+    freeClients.add(socket.id);
+    if (toId) {
+      socket.to(toId).emit('stranger disconnected');
+      socket.emit('stranger disconnected');
+    } else {
+      searchingClients.delete(socket.id);
+    }
+  });
+
+  socket.on('online', cb => {
+    return cb(io.sockets.clients().server.eio.clientsCount);
+  });
+
+  socket.on('disconnect', () => {
+    if (freeClients.has(socket.id)) return freeClients.delete(socket.id);
+    if (searchingClients.has(socket.id)) return searchingClients.delete(socket.id);
+    socket.broadcast.emit('online', io.sockets.clients().server.eio.clientsCount);
+  });
+});
