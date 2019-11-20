e5b431e41fe6f30e587919fb89e0dc94c4f79772
Narek Avagyan <ava.narek@gmail.com>
2019-11-20 23:57:24 +0300

    +

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const path = require('path');
-const app = require('express')();
-const server = require('http').Server(app);
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const port = process.env.PORT || 3000;
 
